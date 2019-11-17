a3edf12d727adfa874f5f61bae90bf1982e22187
Anton Telechev <anton.teleshev@wanadoo.fr>
2019-11-17 20:25:45 +0100

    Render complete static grid with Material-UI (#1)
    * Add gitignore
    
    * Add project config files
    
    * Configure examples
    
    * Add stubs for main part
    
    * Render raw table with material-ui
    
    * Improve table styling

---
+++ src/GridData.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+export class GridData extends React.Component {
+
+  render() {
+    const classes = this.props.useStyles;
+    return (
+      <TableBody>
+        {this.props.rowData.map((row, iRow) => {
+          return (
+            <TableRow key={iRow}>
+              {row.map((cellValue, iCell) => {
+                return <TableCell key={iCell} className={classes.tablecell}>{cellValue}</TableCell>;
+              })}
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    );
+  }
+
+}
+
+GridData.propTypes = {
+  rowData: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.any)).isRequired
+};
+
+export default GridData;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import TournamentGridRenderer from './TournamentGridRenderer';
+
+// Makes the tool available in global scope
+window.TournamentGridRenderer = TournamentGridRenderer;
+
+export default TournamentGridRenderer;
