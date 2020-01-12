51163fd5d949edaf888101a5497080c24db5df9a
Anton Telechev <anton.teleshev@wanadoo.fr>
2020-01-12 11:51:18 +0100

    Upgrade D3 and support blank lines in CSV (#9)
    * Use more recent D3, support blank lines in CSV

--- src/components/csv/load-csv.ts
+++ src/components/csv/load-csv.ts
@@ -1,15 +1,20 @@
-import * as d3 from 'd3';
+import * as d3Select from 'd3-selection';
+import * as d3Dsv from 'd3-dsv';
 import { DataManager } from './data-manager';
 
+const isCsvEmptyLineArray = (row: Array<any>): boolean => {
+  return row && row.length === 1 && row[0] === '';
+};
+
 export const loadCsv = (idCsvWrapper: string): DataManager => {
-  const csvElement = d3.select(`#${idCsvWrapper}`);
-  if (!csvElement || !csvElement[0] || !csvElement[0][0]) {
+  const csvElement = d3Select.select(`#${idCsvWrapper}`);
+  if (csvElement.empty()) {
     throw Error(`Target element with id='${idCsvWrapper}' not found!`);
   }
   const rawCsv = csvElement.text();
-  const parsedCSV: Array<any> = Array.from(d3.csv.parseRows(rawCsv)).filter(
-    (row: Array<any>) => row && row.length > 0
-  );
+  const parsedCSV: Array<any> = d3Dsv
+    .csvParseRows(rawCsv)
+    .filter((row: Array<any>) => row && !isCsvEmptyLineArray(row));
   if (!parsedCSV || parsedCSV.length < 2) {
     throw Error('No CSV data found!');
   }
