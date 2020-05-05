5a495f6a7afb30cf469e1b91cb33b50c94e9d8b7
Anton Telechev <anton.teleshev@wanadoo.fr>
2020-05-05 19:05:58 +0200

    Adjust load-csv.ts

--- src/components/csv/load-csv.ts
+++ src/components/csv/load-csv.ts
@@ -1,6 +1,7 @@
 import * as d3Select from 'd3-selection';
 import * as d3Dsv from 'd3-dsv';
 import { DataManager } from './data-manager';
+import { buildColumn } from '../columns/column-factory';
 
 const isCsvEmptyLineArray = (row: Array<any>): boolean => {
   return row && row.length === 1 && row[0] === '';
@@ -19,7 +20,7 @@ export const loadCsv = (idCsvWrapper: string): DataManager => {
     throw Error('No CSV data found!');
   }
   const dataManager = new DataManager();
-  dataManager.header = parsedCSV[0];
+  dataManager.columns = (parsedCSV[0] as Array<string>).map((colName, index) => buildColumn(colName, index));
   dataManager.data = parsedCSV.slice(1, parsedCSV.length);
   return dataManager;
 };
